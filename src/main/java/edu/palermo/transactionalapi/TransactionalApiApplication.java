20fd918179997735d50bfbf8f8cc23960a48b0e3
damokravetz <kravetzdamian@gmail.com>
2021-03-11 23:20:12 -0300

    initial commit

---
+++ src/main/java/edu/palermo/transactionalapi/TransactionalApiApplication.java
@@ -0,0 +1,13 @@
+package edu.palermo.transactionalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/palermo/transactionalapi/TransactionalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.palermo.transactionalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
